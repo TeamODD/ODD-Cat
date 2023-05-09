808bc16d05541a8065a958bc9352a5475e43933a
sjw000324@gmail.com <sjw000324@gmail.com>
2023-05-09 22:30:28 +0900

    chore : merge each commits

--- Assets/Scripts/UI/GameOver/GameOverScreen.cs
+++ Assets/Scripts/UI/GameOver/GameOverScreen.cs
@@ -6,12 +6,12 @@ using TMPro;
 
 public class GameOverScreen : MonoBehaviour
 {
-    [SerializeField] TMP_InputField name;
+    [SerializeField] TMP_InputField nickname;
 
     // Start is called before the first frame update
     public void onSave()
     {
-        GameMgr.GetIns._Nickname = name.text;
+        GameMgr.GetIns._Nickname = nickname.text;
         GameMgr.GetIns.SaveData();
         SceneManager.LoadScene("RankingScene");
     }
